82698b2b016f030baaef370971c09d45d4329a33
zzzonked <yaroslavkuts@gmail.com>
2019-12-14 12:32:41 +0200

    add ability to serve static content

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const fastify = require('fastify');
+const path = require('path');
 
 const PORT = 8080;
 
 const server = fastify({ logger: true });
 
+server.register(require('fastify-static'), {
+  root: path.join(__dirname, '/build'),
+})
+
 server.get('/api/v1/check', (request, reply) => {
   return reply.send({ status: 'ok' });
+})
+
+server.get('/', function (req, reply) {
+  return reply.sendFile('index.html');
 })
 
 server.listen(PORT, (error) => {
